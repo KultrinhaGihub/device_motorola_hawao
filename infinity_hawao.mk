88e00e16f3a239e5b862c1bf26a16550c6abd68d
Kultrinhaa_ <pedrootaviopluss@proton.me>
2026-08-21 01:54:35 -0300

    hawao: bring up for infinity

--- infinity_hawao.mk
+++ infinity_hawao.mk
@@ -10,11 +10,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/motorola/hawao/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Project Infinity X stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+
+# Project Infinity X configs
+INFINITY_MAINTAINER := Kultrinhaa_
+WITH_GAPPS := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_hawao
+PRODUCT_NAME := infinity_hawao
 PRODUCT_DEVICE := hawao
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g42
